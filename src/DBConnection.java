d73eb2e183445745d51801113ec5ab7623fafcfb
Nada Fathy Ali <48325643+nada-fathy@users.noreply.github.com>
2022-05-24 01:41:58 +0200

    Add files via upload

--- src/DBConnection.java
+++ src/DBConnection.java
@@ -2,12 +2,16 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
 import java.sql.*;
 
 public class DBConnection {
     public static Connection getConnection() throws SQLException, Exception {
         // JDBC driver name and database URL
-        final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+        final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
         final String DB_URL = "jdbc:mysql://localhost:3306/?user=root";
 
         //  Database credentials
@@ -19,4 +23,25 @@ public class DBConnection {
 
         return conn;
     }
+    public static void main(String[]args) throws SQLException, Exception {
+    	User user = new User();
+    	System.out.println(user.login("1","11"));
+    	Doctor dr = (Doctor)user.get_user();
+    	LocalDate date = LocalDate.parse("2022-06-22");
+    	LocalTime time = LocalTime.parse("06:00:00");
+    	//System.out.println(dr.add_appointment(date,time));
+    	//receptionist
+    	User user2 = new User();
+    	System.out.println(user2.login("2","11"));
+    	Receptionist res = (Receptionist) user2.get_user();
+    	List<Appointment>l = new ArrayList<>();
+    	l= res.view_empty_appointment();
+    	System.out.println(l.size());
+    	for(Appointment p : l) {
+    		System.out.println(p.doctor_id+"  "+p.doctor_name);
+    	}
+    	//System.out.println(res.add_patient("3", "f", "Sayd", "56", "Egypt"));
+    	//System.out.println(res.reserve_appointment("1",date,time,"3","Sayd"));
+    }
+    
 }
--- src/User.java
+++ src/User.java
@@ -1,17 +1,56 @@
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
 public class User {
     protected String ID;
     protected String name;
     protected String password;
     protected boolean isDoctor;
+    User user;
 
 
 
-    public void login(String ID,String password){
+    public boolean login(String ID,String password) throws SQLException, Exception{
         this.ID = ID;
         this.password = password;
         //validate user (check if ID exists, if the password true)
-        //this.name = name
-        //check if isDoctor ---> isDoctor = true
+        Statement stmt = null;
+        Connection conn = DBConnection.getConnection();
+
+        try {
+            stmt = conn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+            String sql;
+
+            sql = "SELECT * FROM clinic.User WHERE ID = \'"+ ID +"\' and Password = \'"+password+"\' ;";
+
+            ResultSet rs = stmt.executeQuery(sql);
+          
+            rs.last();
+            if(rs.getRow()!=1) {
+            	System.out.println("exit here!!"+rs.getRow());
+            	return false;
+            }
+            this.name = rs.getString("Name");
+            this.isDoctor = rs.getBoolean("isDoctor");
+            if(this.isDoctor) {
+            	user = new Doctor(this.ID,this.name);
+            }else {
+            	user = new Receptionist(this.ID,this.name);
+            }
+            stmt.close();
+            conn.close();
+        }
+        catch(SQLException err) {
+        	err.printStackTrace();
+        	
+        }
+        return true;
+    }
+    public User get_user() {
+    	return user;
     }
 
 }
