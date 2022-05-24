0594da64e39e1b871c8d28195b766ba3aec5142e
Maria-Onsy <76399367+Maria-Onsy@users.noreply.github.com>
2022-05-24 16:05:32 +0200

    Update Doctor.java

--- src/Doctor.java
+++ src/Doctor.java
@@ -12,7 +12,7 @@ public class Doctor extends User{
 		this.name = name;
 	}
     public boolean add_appointment(LocalDate date, LocalTime time) throws Exception {
-    	if(date.isBefore(LocalDate.now())&&time.isBefore(LocalTime.now())) {
+    	if(date.isBefore(LocalDate.now())||(date.equals(LocalDate.now())&&time.isBefore(LocalTime.now()))) {
     		return false;
     	}
         Statement stmt ;
